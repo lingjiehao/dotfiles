c07da4aff93339ce868e83cbebc1181020dd0af7
Hao Lingjie <michael-hao@foxmail.com>
2020-01-05 02:46:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
+
+cd $HOME
+
+sudo apt install silversearcher-ag \
+	tmux
+
+ln -s $HOME/.dotfiles/vimrc .vimrc
+ln -s $HOME/.dotfiles/tmux.conf tmux.conf
+ln -s $HOME/.dotfiles/gitconfig .gitconfig
+ln -s $HOME/.dotfiles/bashrc .bashrc
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
