c085708740cbd3d1cc24f3c62bb0e9af50383c97
Hao Lingjie <michael-hao@foxmail.com>
2020-01-06 11:28:19 +0800

    Add docker config file

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -s $HOME/.dotfiles/vimrc .vimrc
 ln -s $HOME/.dotfiles/tmux.conf .tmux.conf
 ln -s $HOME/.dotfiles/gitconfig .gitconfig
 ln -s $HOME/.dotfiles/bashrc .bashrc
+mkdir -p $HOME/.pip && ln -s $HOME/.dotfiles/pip.conf $HOME/.pip/pip.conf
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
