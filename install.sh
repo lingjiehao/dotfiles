54d91edd418fa403eac19f672e2c0ec421083916
Hao Lingjie <michael-hao@foxmail.com>
2020-01-07 10:02:26 +0800

    Rename ssh_config -> ssh_config.sample

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ ln -s $HOME/.dotfiles/tmux.conf .tmux.conf
 ln -s $HOME/.dotfiles/gitconfig .gitconfig
 ln -s $HOME/.dotfiles/bashrc .bashrc
 mkdir -p $HOME/.pip && ln -s $HOME/.dotfiles/pip.conf $HOME/.pip/pip.conf
-mkdir -p $HOME/.ssh && ln -s $HOME/.dotfiles/ssh_config $HOME/.ssh/config
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
